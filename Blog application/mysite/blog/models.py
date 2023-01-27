b828d9fd5ce68538a5694eb697af35dfe3eb5f82
albertNSBM <albertnsaba2018@gmail.com>
2023-01-27 09:06:40 +0200

    adding built in forms

--- Blog application/mysite/blog/models.py
+++ Blog application/mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 #the post app models
 
@@ -16,7 +17,7 @@ class Post(models.Model):
         PUBLISHED='PB','published'
     
     title=models.CharField(max_length=20)
-    slug=models.CharField(max_length=20)
+    slug=models.CharField(max_length=20,unique_for_date='publish')
     body=models.TextField()
     publish=models.DateTimeField(default=timezone.now)
     created=models.DateTimeField(auto_now_add=True)
@@ -38,4 +39,11 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
+    
 
