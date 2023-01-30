49ef2a7e1d73e1f7f57c5843d919979abb6dbafa
albertNSBM <albertnsaba2018@gmail.com>
2023-01-30 10:02:25 +0200

    introduction to adding taggit

--- Blog application/my_env/Scripts/activate.bat
+++ Blog application/my_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Blog application\my_env
+set VIRTUAL_ENV=D:\Blog-site-in-Django-with-python-\Blog application\my_env
 
 if not defined PROMPT set PROMPT=$P$G
 
